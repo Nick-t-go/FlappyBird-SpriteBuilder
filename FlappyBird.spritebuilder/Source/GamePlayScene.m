3ccbfd0b0b04a3356912cf521151459e9595aa4e
Uzer Y <uzer-y@Uzers-MacBook-Pro.local>
2015-01-10 01:50:31 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -1,8 +1,25 @@
 #import "GamePlayScene.h"
 #import "Character.h"
 #import "Obstacle.h"
+@interface CGPointObject : NSObject
+{
+    CGPoint _ratio;
+    CGPoint _offset;
+    CCNode *__unsafe_unretained _child; // weak ref
+}
+@property (nonatomic,readwrite) CGPoint ratio;
+@property (nonatomic,readwrite) CGPoint offset;
+@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
++(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+-(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
+@end
 
 @implementation GamePlayScene
+CGPoint _cloudParallaxRatio;
+CGPoint _bushParallaxRatio;
+
+CCNode *_parallaxContainer;
+CCParallaxNode *_parallaxBackground;
 
 - (void)initialize
 {
